44e53100efd7d5e265d67ef63561534a0e1f7be0
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-29 17:51:49 -0500

    chapter 4

---
+++ array-object.js
@@ -0,0 +1,22 @@
+// let input = [-1, 5, "cat", false, 10.2, true, "dog"];
+
+ function arrayConverter(array) {
+    let arrayConverter= {};
+    let arrayS = [];
+    let arrayN = [];
+    let arrayB = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == "String"){
+           arrayS.push(array[i]);
+           arrayConverter.arrayS = arrayS;
+        } else if (array[i] == "number") {
+            arrayN.push(array[i]);
+            arrayConverter.arrayN = arrayN;
+        }else {
+            arrayB.push(array[i]);
+            arrayConverter.arrayB = arrayB;
+        }
+    }
+    return arrayConverter;
+
+ }
